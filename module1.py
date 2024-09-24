eb866026209e82e3679406d635aac1406627809f
default <heman28102002@gmail.com>
2024-09-24 15:29:47 +0000

    module2

--- module1.py
+++ module1.py
@@ -1,2 +1,15 @@
 #display statement
 print("hello world")
+
+#python uses
+"""web development (server-side),
+software development,
+mathematics,
+system scripting.
+"""
+"""Python can be used on a server to create web applications.
+Python can be used alongside software to create workflows.
+Python can connect to database systems. It can also read and modify files.
+Python can be used to handle big data and perform complex mathematics.
+Python can be used for rapid prototyping, or for production-ready software development.
+"""
