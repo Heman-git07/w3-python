ce71fb8f29edf2aa5ffd442eda9e0f3ddb3973fa
default <heman28102002@gmail.com>
2024-09-24 15:35:50 +0000

    module3

--- module1.py
+++ module1.py
@@ -13,3 +13,17 @@ Python can connect to database systems. It can also read and modify files.
 Python can be used to handle big data and perform complex mathematics.
 Python can be used for rapid prototyping, or for production-ready software development.
 """
+
+#to check whether python will installed or not
+"""python --version"""
+
+#The way to run a python file is like this on the command line
+
+"""python filename.py"""
+
+#Check the Python version of the editor
+
+import sys
+
+print(sys.version)
+
